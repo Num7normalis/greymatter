60322d7a660fab95993e21f45bfcd3dfddbeeba4
Num7 <47701832+Num7normalis@users.noreply.github.com>
2024-03-19 15:07:57 -0400

    Incremented version numbers to 1.2

--- greymatter/style.conf.php
+++ greymatter/style.conf.php
@@ -3,7 +3,7 @@
 Style Name: Grey Matter
 Style URI: https://num7.paranormalis.com
 Description: Default Grey Matter style
-Version: 1.0
+Version: 1.2
 Author: Num7
 Author URI: https://num7.paranormalis.com
 */
@@ -15,7 +15,7 @@ Author URI: https://num7.paranormalis.com
 	$style['author'] = 'Num7';
 	$style['www'] = 'https://num7.paranormalis.com';
 	
-	$style['version'] = 1.1;
+	$style['version'] = 1.2;
 		
 	$style['style_def'] = 'style.css';
 	$style['style_admin'] = 'admin.css';
--- theme.conf.php
+++ theme.conf.php
@@ -3,7 +3,7 @@
 Theme Name: Grey Matter
 Theme URI: https://num7.paranormalis.com
 Description: A modern FlatPress theme in tones of dark grey
-Version: 1.0
+Version: 1.2
 Author: Num7
 Author URI: https://num7.paranormalis.com
 */
@@ -15,7 +15,7 @@ Author URI: https://num7.paranormalis.com
 	$theme['description'] = 'A modern FlatPress theme in tones of dark grey';
 	
 	
-	$theme['version'] = 1.1;
+	$theme['version'] = 1.2;
 		
 	$theme['style_def'] = 'style.css';
 	$theme['style_admin'] = 'admin.css';
